a90fd4e6a183025aa7d315486fa8054eca3c5cda
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-01-15 13:08:32 +0530

    publish image tags to dockerhub (#10)
    In addition to latest, publish version and slug tags to dockerhub

---
+++ .github/workflows/push-to-dockerhub.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly version=$(cat VERSION)
+readonly git_branch=${1:11} # drop 'refs/head/' prefix
+readonly git_sha=$(git rev-parse HEAD)
+readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
+readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
+
+docker tag projectriff/dev-utils:latest projectriff/dev-utils:${version}
+docker tag projectriff/dev-utils:latest projectriff/dev-utils:${slug}
+
+docker push projectriff/dev-utils:${version}
+docker push projectriff/dev-utils:${slug}
